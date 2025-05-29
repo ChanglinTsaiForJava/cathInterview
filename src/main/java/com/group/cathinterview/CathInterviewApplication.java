40fa16d23ac784523faf2b031afd5ae007a07a56
steve <changlin.stevetsai@gmail.com>
2025-05-29 16:39:26 +0800

    Initial commit

---
+++ src/main/java/com/group/cathinterview/CathInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.group.cathinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CathInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/cathinterview/CathInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.cathinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CathInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
